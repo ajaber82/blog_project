fa7d6a6b4fb0c0ac815b80998e1db0d50420d4f3
ajaber82 <ajaber82@gmail.com>
2017-11-03 01:16:47 -0500

    git category full

--- test/com/letspeer/test/CategoryDaoTest.java
+++ test/com/letspeer/test/CategoryDaoTest.java
@@ -73,45 +73,44 @@ public class CategoryDaoTest {
 		
 	}
 	
-	/*@Test
+	@Test
 	public void test4_Update() {
 		
 		try {
+			Category c = new Category();
+		
+			c.setId(categoryId);
+			c.setCategoryName("sport");
+		
+			c.setDeleted(false);
+		
 			
-			User u = new User();
-			u.setId(userId);
-			u.setAboutMe("something");
-			u.setDeleted(false);
-			u.setEmail("u@xyz.com");
-			u.setFirstName("fName");
-			u.setLastName("lName");
-			u.setPassword("pwd");
-			u.setProfilePicture("pic1.jpg");
-			
-			UserDao dao = new UserDaoImpl("jdbc:mysql://127.0.0.1:3306/blog_db?autoReconnect=true&useSSL=false" , 
+			CategoryDao dao = new CategoryDaoImpl("jdbc:mysql://127.0.0.1:3306/blog_db?autoReconnect=true&useSSL=false" , 
 					"root" , "12345678");
 			
-			Boolean result = dao.updateUser(u) ;
+			Boolean result = dao.updateCategory(c) ;
 			assertFalse(result);
-			u = dao.getUserById(userId) ; 
-			assertNotNull(u);
-			assertTrue(u.getEmail().equals("u@xyz.com"));
+			
+			c= dao.getCategoryById(categoryId);
+			assertNotNull(c);
+		
 		}catch(Exception exp) {
 			exp.printStackTrace();
 		}	
 	}
 	
 	@Test
-	public void test5_deleteUser() {
-		UserDao dao = new UserDaoImpl("jdbc:mysql://127.0.0.1:3306/blog_db?autoReconnect=true&useSSL=false" , 
+	public void test5_deleteCategory() {
+		CategoryDao dao = new CategoryDaoImpl("jdbc:mysql://127.0.0.1:3306/blog_db?autoReconnect=true&useSSL=false" , 
 				"root" , "12345678");
-		dao.deleteUser(userId);
-		
-		User u = dao.getUserById(userId) ; 
-		assertNull(u);
+		dao.deleteCategory(categoryId);
+	    Category c = dao.getCategoryById(categoryId);
 		
 		
-	}*/
+		assertNull(c);
+		
+		
+	}
 	
 
 }
